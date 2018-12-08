bd520fcb4436cc3ef948c8f3f15880806991a100
Brandon Murdoch <brandon.murdoch@argos.co.uk>
2018-12-08 13:17:47 +0000

    Now to find the hour

--- 4-1.py
+++ 4-1.py
@@ -7,6 +7,8 @@ sorted_lines = sorted(input_file.readlines())
 
 
 sleep_dicts = {}
+sleep_count = {}
+
 guard = ""
 sleep_start = ""
 sleep_end = ""
@@ -15,19 +17,25 @@ for line in sorted_lines:
     if("Guard" in line):
         guard = line[line.index("Guard #")+7:line.index(" begins")]
     if("falls asleep" in line):
-        sleep_start = line[line.index("[")+1:line.index("]")]
+        sleep_start = line[line.index(":")+1:line.index("]")]
     if("wakes up" in line):
-        sleep_end = line[line.index("[")+1:line.index("]")]
-        print guard + "slept from " + sleep_start + " to " + sleep_end
+        sleep_end = line[line.index(":")+1:line.index("]")]
         if (guard not in sleep_dicts):
             sleep_dicts[guard] = []
+            sleep_count[guard] = 0
         sleep_dicts[guard].append("From " + sleep_start + " To " + sleep_end)
+        sleep_count[guard] = sleep_count[guard] + (int(sleep_end) - int(sleep_start) - 1)
 
-print sleep_dicts
+# find the biggest sleeper:
+biggest_sleeper = ""
+max_sleep = 0;
+for g in sleep_count:
+    if (sleep_count[g] > max_sleep):
+        max_sleep = sleep_count[g]
+        biggest_sleeper = g
 
-# Now I have a dictionary of guards, with all the times they sleep
-{
-    '3251': ['From 1518-02-03 00:05 To 1518-02-03 00:34'],
-    '479': ['From 1518-02-04 00:14 To 1518-02-04 00:27', 'From 1518-02-04 00:42 To 1518-02-04 00:58'],
-    '137': ['From 1518-02-01 00:39 To 1518-02-01 00:55']
-}
+print "The biggest sleeper is: " + biggest_sleeper
+
+# Now find the most common time asleep
+
+print sleep_dicts[biggest_sleeper]
